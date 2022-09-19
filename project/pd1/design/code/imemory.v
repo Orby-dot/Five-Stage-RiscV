84dafbac9358a2c5dc36aee674652d626d8e8b6a
Nicolas Bao <n3bao@uwaterloo.ca>
2022-09-19 13:56:52 -0400

    Added write function. May not be right

--- project/pd1/design/code/imemory.v
+++ project/pd1/design/code/imemory.v
@@ -1,10 +1,15 @@
-module imemory(
+module imemory
+#(parameter  MEM_DEPTH = 32'd1048576)
+(
   input wire              clock,
   input wire    [31:0]    address,
   input wire    [31:0]    read_write,
   input wire    [31:0]    data_in,
   output wire   [31:0]    data_out
 );
+
+//8 byte chunks
+reg [7:0] memory [MEM_DEPTH]; //actual main memory
 
   // load binary
   initial begin
@@ -24,6 +29,15 @@ module imemory(
   if (address) begin
     if (read_write) begin
       //write
+      //so this is kinda messy because of little endian
+      /* i think its possible to do something like:
+      memory[address + 3 : address] = data
+      ^would be easy and pretty cool but idk just yet
+      */
+      memory[address] <= data_in[7:0];
+      memory[address + 1] <= data_in[15:8];
+      memory[address+ 2] <= data_in[23:16];
+      memory[address + 3] <= data_in[31:24];
     end else begin
       // read
     end
