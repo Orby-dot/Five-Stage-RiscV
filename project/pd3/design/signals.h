1155ade391a3e056b7a3915fc189048b5e1daed6
Raven <r4sim@uwaterloo.ca>
2022-10-04 11:35:43 -0400

    start of signals.h, will need to disambiguate some probes

--- project/pd3/design/signals.h
+++ project/pd3/design/signals.h
@@ -3,26 +3,26 @@
  * and replace ??? with actual wires */
 // ----- signals -----
 // You will also need to define PC properly
-`define F_PC                ???
-`define F_INSN              ???
+`define F_PC                address
+`define F_INSN              data_out
 
-`define D_PC                ???
-`define D_OPCODE            ???
-`define D_RD                ???
-`define D_RS1               ???
-`define D_RS2               ???
-`define D_FUNCT3            ???
-`define D_FUNCT7            ???
-`define D_IMM               ???
-`define D_SHAMT             ???
+`define D_PC                address
+`define D_OPCODE            opcode
+`define D_RD                rd
+`define D_RS1               rs1
+`define D_RS2               rs2
+`define D_FUNCT3            funct3
+`define D_FUNCT7            funct7
+`define D_IMM               imm
+`define D_SHAMT             shamt
 
-`define R_WRITE_ENABLE      ???
+`define R_WRITE_ENABLE      write_enable
 `define R_WRITE_DESTINATION ???
-`define R_WRITE_DATA        ???
+`define R_WRITE_DATA        data_rd
 `define R_READ_RS1          ???
 `define R_READ_RS2          ???
-`define R_READ_RS1_DATA     ???
-`define R_READ_RS2_DATA     ???
+`define R_READ_RS1_DATA     data_rs1
+`define R_READ_RS2_DATA     data_rs2
 
 `define E_PC                ???
 `define E_ALU_RES           ???
