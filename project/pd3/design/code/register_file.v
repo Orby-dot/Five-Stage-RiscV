bffc1cafa964f76fe98ea8637face7578f2748d5
Nicolas Bao <n3bao@uwaterloo.ca>
2022-10-04 12:24:01 -0400

    made read on refile combination

--- project/pd3/design/code/register_file.v
+++ project/pd3/design/code/register_file.v
@@ -16,30 +16,14 @@ module register_file
 
 reg [30:0] reg_mem [31:0]; //reg file
 
-  always @(posedge clock)begin
+assign (addr_rs1 == 0) ? data_rs1 = 0:data_rs1 = reg_mem[addr_rs1]; //rs1
+assign (addr_rs2 == 0) ? data_rs2 = 0:data_rs2 = reg_mem[addr_rs2]; //rs2
+
+  always @(*)begin
     if (read_write) begin
       //write
       memory[addr_rd] = data_rd;
     end
-  end
-
-  //rs1
-  always @(*) begin
-    // $display("Addr %h", address);
-    // $display("TrueAddr %h", trueAddr);
-    if(addr_rs1 == 0)
-        data_rs1 = 0;
-    else
-        data_rs1 = reg_mem[addr_rs1];
-  end
-  //rs2
-  always @(*) begin
-    // $display("Addr %h", address);
-    // $display("TrueAddr %h", trueAddr);
-    if(addr_rs2 == 0)
-        data_rs2 = 0;
-    else
-        data_rs2 = reg_mem[addr_rs2];
   end
 
 endmodule
