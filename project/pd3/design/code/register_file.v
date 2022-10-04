ac0b90ed44ae23f9820526a5ccf69e47abbd94b4
Raven <r4sim@uwaterloo.ca>
2022-10-04 11:47:47 -0400

    some comments and useful code to modify

--- project/pd3/design/code/register_file.v
+++ project/pd3/design/code/register_file.v
@@ -10,4 +10,25 @@ module register_file
   output wire   [31:0]    data_rs2
 );
 
+
+  // reads are combinational, writes are sequential
+  // are we accessing registers or imemory?
+
+  // // combinational read (from imemory.v)
+  // assign data_out[7:0] = (memory[trueAddr] & {8{!read_write}} );
+  // assign data_out[15:8] = (memory[trueAddr + 1] & {8{!read_write}} );
+  // assign data_out[23:16] = (memory[trueAddr + 2] & {8{!read_write}} );
+  // assign data_out[31:24] = (memory[trueAddr + 3] & {8{!read_write}} );
+
+
+  // // sequential write (slightly modified from imemory.v)
+  // should be posedge clk according to lecture slides (04_datapath_control slide 14)
+  // always @(posedge clock) begin  
+  //   if (write_enable) begin
+  //     memory[trueAddr] = data_in[7:0];
+  //     memory[trueAddr + 1] = data_in[15:8];
+  //     memory[trueAddr+ 2] = data_in[23:16];
+  //     memory[trueAddr + 3] = data_in[31:24];
+  //   end
+  // end
 endmodule
