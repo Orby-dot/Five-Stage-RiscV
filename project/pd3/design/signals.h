9b88c6e6e59a3024a7256058b6c96a916aa258d1
Raven <r4sim@uwaterloo.ca>
2022-10-05 09:37:45 -0400

    signals and design files

--- project/pd3/design/signals.h
+++ project/pd3/design/signals.h
@@ -8,25 +8,25 @@
 
 `define D_PC                address
 `define D_OPCODE            opcode
-`define D_RD                rd
-`define D_RS1               rs1
-`define D_RS2               rs2
+`define D_RD                addr_rd
+`define D_RS1               addr_rs1
+`define D_RS2               addr_rs2
 `define D_FUNCT3            funct3
 `define D_FUNCT7            funct7
 `define D_IMM               imm
 `define D_SHAMT             shamt
 
 `define R_WRITE_ENABLE      write_enable
-`define R_WRITE_DESTINATION ???
+`define R_WRITE_DESTINATION addr_rd
 `define R_WRITE_DATA        data_rd
-`define R_READ_RS1          ???
-`define R_READ_RS2          ???
+`define R_READ_RS1          addr_rs1
+`define R_READ_RS2          addr_rs2
 `define R_READ_RS1_DATA     data_rs1
 `define R_READ_RS2_DATA     data_rs2
 
-`define E_PC                ???
-`define E_ALU_RES           ???
-`define E_BR_TAKEN          ???
+`define E_PC                e_pc
+`define E_ALU_RES           alu_out
+`define E_BR_TAKEN          brn_tkn
 
 // ----- signals -----
 
--- project/pd3/verif/scripts/design.f
+++ project/pd3/verif/scripts/design.f
@@ -1,2 +1,7 @@
 register_file.v
 pd.v
+ALU.v
+branch_comp.v
+imemory.v
+pc_counter.v
+control.v
