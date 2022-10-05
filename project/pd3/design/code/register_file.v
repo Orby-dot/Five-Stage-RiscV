2edf0236067aa9377d5a6f4c93b308f03a116032
Nicolas Bao <n3bao@uwaterloo.ca>
2022-10-05 13:42:05 -0400

    fixed reg file

--- project/pd3/design/code/register_file.v
+++ project/pd3/design/code/register_file.v
@@ -18,12 +18,14 @@ reg [31:0] reg_mem [30:0]; //reg file
 
 assign data_rs1 = (addr_rs1 == 0) ?  0: reg_mem[addr_rs1-1]; //rs1
 assign data_rs2 = (addr_rs2 == 0) ?  0: reg_mem[addr_rs2-1]; //rs2
-
-  always @(*)begin
+initial begin
+  reg_mem[1] = `MEM_DEPTH + 32'h01000000;
+end
+  always @(posedge clock)begin
     if (write_enable) begin
       //write
       if(addr_rd != 0)
-        reg_mem[addr_rd -1] = data_rd;
+        reg_mem[addr_rd -1] <= data_rd;
     end
   end
 
